5b59359ed90fb760ea3d108d51d8ccc1cf1b6806
lijas <lijason991@gmail.com>
2025-10-07 21:15:02 -0400

    added comments

--- src/main/java/com/pluralsight/BedtimeStories.java
+++ src/main/java/com/pluralsight/BedtimeStories.java
@@ -1,18 +1,23 @@
 package com.pluralsight;
 
+// importing readers and Scanner
 import java.io.*;
 import java.util.Scanner;
 
 public class BedtimeStories {
 
+    // declares public Scanner
     public static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
+        // creates isRunning boolean to keep looping menu
         boolean isRunning = true;
 
+        // while loop
         while (isRunning) {
 
+            // prints out menu with bedtime story choices
             System.out.println("""
                     Which bedtime story would you like to read?
                     (1) Goldilocks
@@ -21,8 +26,10 @@ public class BedtimeStories {
                     (4) I'm tired, let's say good night
                     """);
 
+            // calls getValidNumber method to ensure that the input is an integer
             int storyNum = getValidNumber();
 
+            // switch case leading to printStory method with different txt files, an exiting case, and a default
             switch (storyNum) {
                 case 1:
                     printStory("goldilocks.txt");
@@ -45,6 +52,7 @@ public class BedtimeStories {
 
     }
 
+    // getValidNumber ensures correct integer input
     public static int getValidNumber() {
 
         // initializes inputNumber and boolean badInput
@@ -75,17 +83,23 @@ public class BedtimeStories {
 
     }
 
-    public static void printStory(String storyName) throws FileNotFoundException {
+    // used to print each story
+    public static void printStory(String storyName) {
 
+        // uses try/catch to avoid problems in case of something being wrong with the file
         try (BufferedReader bufReader = new BufferedReader(new FileReader(storyName))) {
 
+            // creates a String called storyLine and a counter variable
             String storyLine;
             int storyLineCounter = 0;
 
+            // uses while loop to go through file, printing out each line as it enters bufferedReader
+            // numbers storyLines to the left
             while ((storyLine = bufReader.readLine()) != null) {
                 System.out.println(++storyLineCounter + ". " + storyLine);
             }
 
+        // has a catch block in case something is wrong with the file, printing out error message
         } catch (Exception e) {
 
             System.out.println("Sorry, I'm having trouble reading this one, let's try another.");
